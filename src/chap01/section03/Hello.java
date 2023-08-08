087df9bdff3deb6767750a260c5463fd98cb51df
max <inews2432@gmail.com>
2023-08-09 04:16:02 +0900

    Initial commit

---
+++ src/chap01/section03/Hello.java
@@ -0,0 +1,12 @@
+package chap01.section03;
+
+public class Hello {
+    public static void main(String[] args) {
+       int age = 17;
+       int salary = 5000;
+
+       System.out.println(age);
+       System.out.println(age + salary);
+    }
+}
+
