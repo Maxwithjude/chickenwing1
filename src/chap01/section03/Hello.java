96a0ab197e4c317f531eb8485280d8f77b7916c8
max <inews2432@gmail.com>
2023-12-18 16:01:37 +0900

    2023-11-14

--- src/chap01/section03/Hello.java
+++ src/chap01/section03/Hello.java
@@ -1033,3 +1033,33 @@ import java.io.IOException;
          System.out.println(a);
      }
  }*/
+/*
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Hello {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        int sum = 0;
+        for (byte value : br.readLine().getBytes()){
+            sum += (value - '0');
+
+
+        }
+        System.out.println(sum);
+    }
+}*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Hello {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+
+    }
+}
