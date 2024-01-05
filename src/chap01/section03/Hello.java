be41ef1e79dc4c475409aa78e41425fd60a1819e
max <inews2432@gmail.com>
2024-01-06 01:50:02 +0900

    2023-11-14

--- src/chap01/section03/Hello.java
+++ src/chap01/section03/Hello.java
@@ -1578,6 +1578,7 @@ public class Hello {
     }
 }*/
 
+/*
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -1609,5 +1610,29 @@ public class Hello{
         StringBuffer stringBuffer = new StringBuffer(answer);
 
         System.out.println(stringBuffer.reverse().toString());
+    }
+}*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Hello{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int unit[] = {25, 10, 5, 1};
+        int result[][] = new int[n][4];
+
+        for (int i =0; i < n; i++){
+            int input = Integer.parseInt(br.readLine());
+            for (int j = 0; j < 4; j++){
+                result[i][j] = input / unit[j];
+                sb.append(result[i][j]).append(" ");
+                input %= unit[j];
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 }
