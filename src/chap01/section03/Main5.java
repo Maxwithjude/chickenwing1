c0d9ce24f7592db83aef8936ae2b20bbaa115da3
MaxwithJude <inews2432@gmail.com>
2025-10-19 23:42:09 +0900

    BOJ 2441

--- src/chap01/section03/Main5.java
+++ src/chap01/section03/Main5.java
@@ -14,7 +14,6 @@ public class Main5 {
         int [] arr = new int[n+1];
         arr[0] = 0;
         arr[1] = 1;
-        int sum = 0;
 
         for(int i = 2; i <= n; i++){
             arr[i] = arr[i-1] + arr[i-2];
---
+++ src/chap01/section03/Main6.java
@@ -0,0 +1,10 @@
+package chap01.section03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main6 {
+    public static void main (String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
